ed0598689fad131e3f69f9c7ebcb42027b18c530
sardor-abdusattorov <s.abdusattorov@acdf.uz>
2026-05-15 14:27:58 +0500

    updated files

---
+++ resources/views/pages/contacts.blade.php
@@ -0,0 +1,198 @@
+<div class="section__secondary">
+    <div class="my-container">
+        <div class="block__wrap">
+
+            <nav class="breadcrumbs">
+                <ul class="breadcrumbs__ul">
+                    <li class="breadcrumbs__item">
+                        <a href="https://perfectum.uz" class="breadcrumbs__link">Asosiy</a>
+                    </li>
+                    <li class="breadcrumbs__item">
+                        <a href="#!" class="breadcrumbs__link">Kontaktlar</a>
+                    </li>
+                </ul>
+            </nav>
+
+            <h5 class="card__date">
+                19 May, 2022
+            </h5>
+
+            <h2 class="block__title mb-20">Kontaktlar</h2>
+
+            <div class="content__text">
+                <p>
+                </p>
+                <figure class="table">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td colspan="2">OFISNI ISH TARTIBI</td>
+                        </tr>
+                        <tr>
+                            <td rowspan="2">ish kunlari<br>&nbsp;</td>
+                            <td><span style="color:rgb(0,0,0);">&nbsp;Bosh ofis (Toshkent sh.) -</span> <span
+                                    style="color:rgb(230,0,0);"><strong>08:00 </strong>-<strong>
+                                                19:00</strong></span></td>
+                        </tr>
+                        <tr>
+                            <td>Boshqa ofislar &nbsp;- <span style="color:rgb(230,0,0);"><strong>09:00
+                                            </strong>-<strong> 18:00</strong></span></td>
+                        </tr>
+                        <tr>
+                            <td>shanba</td>
+                            <td><span style="color:rgb(230,0,0);"><strong>10:00&nbsp;</strong>-<strong>
+                                                17:00</strong></span></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </figure>
+                <p>&nbsp;</p>
+                <figure class="table">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <td>MANZIL</td>
+                            <td>TELEFON</td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:rgb(102,102,102);">MARKAZIY OFIS</span></td>
+                            <td>100060, Oʻzbekiston,<br>Toshkent shahri, Taras Shevchenko koʻchasi, 21-uy</td>
+                            <td><span style="color:rgb(102,102,102);">+998983051111,</span><br><span
+                                    style="color:rgb(102,102,102);">+998981270077,</span><br><span
+                                    style="color:rgb(102,102,102);">«Perfectum» mobil telefondan: 077 (bepul) va
+                                            099 - 84,20 so'm / daq.</span></td>
+                        </tr>
+                        <tr>
+                            <td>CHILONZOR OFISI</td>
+                            <td><span style="color:rgb(102,102,102);">Gagarina koʻch., 40 uy</span></td>
+                            <td><span style="color:rgb(102,102,102);">+998981270022</span></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </figure>
+                <p>&nbsp;</p>
+                <figure class="table">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td colspan="3"><span style="color:rgb(0,0,0);">VILOYATDAGI OFISLAR</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">ANDIJON</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Boburshox
+                                            prospekti 22.</span></td>
+                            <td rowspan="13"><br><br><br><br><br><br><br><br><br><br><br><br><br><span
+                                    style="color:rgb(102,102,102);">+998983051111</span><br><span
+                                    style="color:rgb(102,102,102);">&nbsp;</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">BUXORO</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Mustaqillik
+                                            ko`chasi. 28 uy 2 blok 4. Mo`ljal; k/t «Buxoro»)</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">GULISTON</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Sayxun
+                                            ko`chasi. 17 uy.</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">JIZZAX</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Sharof
+                                            Rashidov. 300</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">QARSHI</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Shodlik
+                                            M.F.Y. Nasaf ko`chasi.&nbsp;</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">QO`QON</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Qo`qon sh,
+                                            Turkiston ko`chasi 136 E uy. Mo`ljal: G`isht ko`prik, yong`in xizmati
+                                            yonida.</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">NAVOIY</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Islom
+                                            Karimov &nbsp;ko`chasi. 103</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">NAMANGAN</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">3-ch tor
+                                            ko`chasi.1</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">NUKUS</span></td>
+                            <td><span
+                                    style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Qoraqalpog`iston
+                                            ko`chasi &nbsp;20 uy.&nbsp;</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">SAMARQAND</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Shoxrux
+                                            Mirzo ko`chasi. 62</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">TERMEZ</span></td>
+                            <td><span style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">Ibn Sino
+                                            ko`chasi. 29 uy. Mo`ljal: temir yo'l vokzali, shahar voenkomat
+                                            ro'parasida</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">URGANCH</span></td>
+                            <td><span style="color:hsl(0,0%,30%);">Paxlavon Maxmud. 17</span></td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:hsl(0,0%,30%);">FARG'ONA</span></td>
+                            <td><span
+                                    style="background-color:rgb(247,247,247);color:hsl(0,0%,30%);">B.Marg`iloniy
+                                            Mashal ko`chasi. 29 M.F.Y.</span></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </figure>
+                <p>&nbsp;</p>
+                <figure class="table">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td colspan="2">MA'LUMOTLAR XIZMATI (CALL-MARKAZ) NING ISH VAQTLARI:</td>
+                        </tr>
+                        <tr>
+                            <td><span style="color:rgb(230,0,0);">07:00</span> dan <span
+                                    style="color:rgb(230,0,0);">23:00</span> gacha</td>
+                            <td>+998983051111, +998981270077, 077 (Perfectum abonentlari uchun bepul),<br>099
+                                (84.20 so'm/daq.)</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </figure>
+                <p>&nbsp;</p>
+                <figure class="table">
+                    <table>
+                        <tbody>
+                        <tr>
+                            <td colspan="2">Fuqarolarning murojaatlarini qabul qilish jadvali.</td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <p style="text-align:center;"><span style="color:rgb(0,0,0);">SESHANBA,
+                                                JUMA</span></p>
+                            </td>
+                            <td><span style="color:rgb(230,0,0);">&nbsp;<strong>10:00&nbsp;</strong>-<strong>
+                                                12:00</strong></span></td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </figure>
+                <p></p>
+            </div>
+
+
+
+        </div>
+
+
+    </div>
+</div>

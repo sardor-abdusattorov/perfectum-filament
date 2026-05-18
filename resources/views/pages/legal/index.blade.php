e1ac6b74d75c925bd7ceba42d8a346ed6d9b74fe
sardor-abdusattorov <s.abdusattorov@acdf.uz>
2026-05-18 18:13:15 +0500

    updated files and addeed services sql

--- resources/views/pages/legal/index.blade.php
+++ resources/views/pages/legal/index.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.primary')
 
-@section('title', $page?->meta_title ?: $page?->title)
-@section('meta_description', $page?->meta_description ?: $page?->description)
+@section('title', $page?->meta_title)
+@section('meta_description', $page?->meta_description)
 
 @section('content')
     <div class="section__secondary section-bg">
         <div class="my-container">
 
-            <x-breadcrumbs :items="[['label' => $page?->title ?: __('app.label.legal_documents_plural')]]" />
+            <x-breadcrumbs :items="[['label' => $page?->title]]" />
 
             <div class="section__top-secondary section__top-flex">
                 <h2 class="block__title">
-                    {{ $page?->title ?: __('app.label.legal_documents_plural') }}
+                    {{ $page?->title }}
                 </h2>
-
-                @if ($page?->description)
-                    <div class="content__text mb-24">
-                        {!! $page->description !!}
-                    </div>
-                @endif
             </div>
 
             <div class="section__grid">
@@ -28,13 +22,11 @@
                             <h3 class="card__title mb-10">
                                 {{ $document->title }}
                             </h3>
-                            @if ($document->description)
-                                <h4 class="card__subtitle">{{ $document->description }}</h4>
-                            @endif
+                            <h4 class="card__subtitle">{{ $document?->description }}</h4>
                         </div>
                     </a>
                 @empty
-                    <p>{{ __('app.label.empty') }}</p>
+                    <p>{{ translator('app', 'empty') }}</p>
                 @endforelse
             </div>
         </div>

db9e7692dcca56c38c4daadaa0081ac5f301382b
sardor-abdusattorov <s.abdusattorov@acdf.uz>
2026-05-18 13:16:41 +0500

    updated files and addeed services sql

--- resources/views/pages/offices.blade.php
+++ resources/views/pages/offices.blade.php
@@ -1,1 +1,12 @@
-<?php
+@php
+    use Illuminate\Support\Facades\Storage;
+@endphp
+
+@extends('layouts.primary')
+
+@section('title', $page?->title)
+@section('meta_description', $page?->meta_description)
+
+@section('content')
+
+@endsection
